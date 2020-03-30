33c27f3d168f2b6b481e274a6babdb1783177c7f
manishchaudhary4316 <manishchaudhary4316@hotmail.com>
2020-03-30 10:41:02 +0530

    updated name

--- h.py
+++ h.py
@@ -1,2 +1,8 @@
-print("helo world")
-#cbjdsvbjndsbvjsj
+def addition(a,b):
+    print(a+b)
+    print(__name__)
+    
+if __name__=="__main__":
+    addition(4,4)
+    print("this is main file")
+    
---
+++ new.py
@@ -0,0 +1,6 @@
+from h import addition
+
+
+print("this is caller file")
+a = addition(7,7)
+print(__name__)
